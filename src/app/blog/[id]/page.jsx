6368d17fd2371425ef2b4b10de2fba6a49005b47
Anna Archive <ytpt@mail.ru>
2023-12-07 02:38:56 -0500

    Created local store in data.js; Fetching data from jsonplaceholder;

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,17 +1,31 @@
 import styles from "./page.module.css";
 import Image from "next/image";
 
-const BlogId = async ({ params }) => {
+async function getData(id) {
+    const res = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${id}`,
+        {cache: "no-store"}
+    );
+    if (!res.ok) {
+        throw new Error("Failed!");
+    }
+
+    return res.json();
+}
+
+const BlogId = async ({ params: {id} }) => {
+    const data = await getData(id);
+
     return (
         <>
             <div className={styles.top}>
                 <div className={styles.info}>
-                    <h1 className={styles.title}>Title</h1>
-                    <p className={styles.desc}>Desc</p>
+                    <h1 className={styles.title}>{data.title}</h1>
+                    <p className={styles.desc}>{data.body}</p>
                     <div className={styles.author}>
                         <Image
-                            src={data.img}
-                            alt=""
+                            src="https://img.freepik.com/free-photo/robot-handshake-human-background-futuristic-digital-age_53876-129770.jpg?w=1380&t=st=1686056951~exp=1686057551~hmac=5da557bfff0192720a1dfa7608b353322afcbfc038886c891ba296fd8fa7e97b"
+                            alt="avatar"
                             width={40}
                             height={40}
                             className={styles.avatar}
@@ -20,7 +34,12 @@ const BlogId = async ({ params }) => {
                     </div>
                 </div>
                 <div className={styles.imageContainer}>
-                    <Image src="" alt="" fill={true} className={styles.image} />
+                    <Image
+                        src="https://img.freepik.com/free-photo/robot-handshake-human-background-futuristic-digital-age_53876-129770.jpg?w=1380&t=st=1686056951~exp=1686057551~hmac=5da557bfff0192720a1dfa7608b353322afcbfc038886c891ba296fd8fa7e97b"
+                        alt=""
+                        fill
+                        className={styles.image}
+                    />
                 </div>
             </div>
             <div className={styles.content}>
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,39 +2,40 @@ import styles from "./page.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
-const Blog = () => {
+async function getData() {
+    const res = await fetch(
+        "https://jsonplaceholder.typicode.com/posts",
+        {cache: "no-store"}
+    );
+    if (!res.ok) {
+        throw new Error("Failed!");
+    }
+
+    return res.json();
+}
+
+const Blog = async () => {
+    const data = await getData();
+
     return (
         <>
-            <Link href="/blog/test" className={styles.container} key="1">
-                <div>
-                    <Image
-                        alt="blog"
-                        width={400}
-                        height={250}
-                        className={styles.image}
-                        src="https://img.freepik.com/free-photo/robot-handshake-human-background-futuristic-digital-age_53876-129770.jpg?w=1380&t=st=1686056951~exp=1686057551~hmac=5da557bfff0192720a1dfa7608b353322afcbfc038886c891ba296fd8fa7e97b"
-                    />
-                </div>
-                <div>
-                    <h1 className={styles.title}>Title</h1>
-                    <p className={styles.desc}>Desc</p>
-                </div>
-            </Link>
-            <Link href="/blog/test2" className={styles.container} key="2">
-                <div>
-                    <Image
-                        alt="blog"
-                        width={400}
-                        height={250}
-                        className={styles.image}
-                        src="https://img.freepik.com/free-photo/robot-handshake-human-background-futuristic-digital-age_53876-129770.jpg?w=1380&t=st=1686056951~exp=1686057551~hmac=5da557bfff0192720a1dfa7608b353322afcbfc038886c891ba296fd8fa7e97b"
-                    />
-                </div>
-                <div>
-                    <h1 className={styles.title}>Title</h1>
-                    <p className={styles.desc}>Desc</p>
-                </div>
-            </Link>
+            {data.map((item) => (
+                <Link href={`/blog/${item.id}`} className={styles.container} key={item.id}>
+                    <div>
+                        <Image
+                            alt="blog"
+                            width={400}
+                            height={250}
+                            className={styles.image}
+                            src="https://img.freepik.com/free-photo/robot-handshake-human-background-futuristic-digital-age_53876-129770.jpg?w=1380&t=st=1686056951~exp=1686057551~hmac=5da557bfff0192720a1dfa7608b353322afcbfc038886c891ba296fd8fa7e97b"
+                        />
+                    </div>
+                    <div>
+                        <h1 className={styles.title}>{item.title}</h1>
+                        <p className={styles.desc}>{item.body}</p>
+                    </div>
+                </Link>
+            ))}
         </>
     );
 };
